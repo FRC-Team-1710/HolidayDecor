ac2e50493486d57ad8ae9f6be21c7764f8784996
Andrew S. Kepych <613ask11@stu.olatheschools.org>
2020-11-03 20:29:35 -0600

    First Commit

--- MyClass.java
+++ MyClass.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class MyClass {
+    static int i = 0;
     public static void main(String args[]) {
+      Scanner myObj = new Scanner(System.in);
+      System.out.println("How Many Times?");
+      int times = myObj.nextInt();
       
+      for (i=0; i < times; i++){
+       row(i,i);
+          
+      }
     }
+    public static void row(int spaces, int stars){
+      for (i=0; i < spaces; i++){
+          System.out.print(" ");
+      }
+      for (i=0; i < stars; i++){
+          System.out.print("*");
+      }
+      System.out.print("\n");
+}
 }
