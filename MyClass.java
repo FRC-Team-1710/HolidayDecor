ca6a040b850e2cd3a2fe0d2576ebf5a198c6c925
Cooper S. Wright <726csw12@stu.olatheschools.org>
2020-11-05 09:31:20 -0600

    babys first commit

--- MyClass.java
+++ MyClass.java
@@ -6,27 +6,47 @@ public class MyClass {
     System.out.println("How Many Times?");
 
     int times = myObj.nextInt();  // Read user input
-    
-     int i = 1;
-    for(i=0; i < times + 1; i++){
-    row(1,i);
-    
+    int trunkwidth = Math.round((times * 2 - 1) / 3);
+    int trunkheight = times / 4;
+    int trunkspace = Math.round((times * 2 - 1) / 3 + 1);
+    if ( (trunkwidth & 1) == 0 ) {
+    trunkwidth = trunkwidth -1 ;
+    } 
+    if (times == 5){
+        trunkspace = trunkspace -1;
+    }
+    if (times == 8){
+        trunkspace = trunkspace -1;
+    }
+    for(int i=0; i < times; i++){
+    row(times - i,i * 2 + 1);
+   // trunk(trunkwidth, trunkwidth);
+    }
+    for(int i = 0; i < times / 4; i++){
+    trunk(trunkspace , trunkwidth);
     }
     
     
     }
     
     public static void row(int spaces, int stars){
-         int i = 0;
-    for(i=0; i < spaces; i++){
-    System.out.print(" ");  
-    }
-    for(i=0; i < stars; i++){
-    System.out.print("*"); 
+         for(int i = 0; i < spaces; i++){
+             System.out.print(" ");
+         }
+    for(int i = 0; i < stars; i++){
+    System.out.print("*");
     }
     System.out.print("\n");
-    
-    system.out.print("*");
     }
+      public static void trunk(int spaces, int stars){
+          for(int i = -1; i < spaces; i++){
+             System.out.print(" ");
+          }
+           for(int i = 0; i < stars; i++){
+    System.out.print("*");
+    }
+    System.out.print("\n");
+      }
     
 }
+     
