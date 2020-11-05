a4c57a8de226253f1741f6a0c31eecc773572b9d
14352 <johndough1432@gmail.com>
2020-11-04 22:09:16 -0600

    working solution

--- MyClass.java
+++ MyClass.java
@@ -1,41 +1,39 @@
-import java.util.Scanner;  // Import the Scanner class
-
+import java.util.Scanner;
+ 
 public class MyClass {
+    
     public static void main(String args[]) {
         
-    Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("How Tall?");
-
-    int times = myObj.nextInt();  // Read user input
-    
-      int i = 0;
-      for(i=0; i < times; i++){
-          row(times-i,i);
-          
-      }
-      
-      
-      
-      
+        Scanner myObj = new Scanner(System.in);
+        int lines = myObj.nextInt();
+        int spaceCount = lines;
+        int starCount = 0;
+        
+        for (int x = 0; x < lines; x++){
+            starsSpaces(starCount+x, spaceCount-x);
+        }
+        
+        for (int x = 0; x < lines/4; x++){
+            starsSpaces(lines/3, lines-lines/3);
+        }
+        
     }
     
-    public static void row(int spaces, int stars){
-          int i = 0;
-          
-          for(i=0; i < spaces; i++){
-              System.out.print(" ");
-          }
-          
-          for(i=0; i < stars; i++){
-              System.out.print("*");
-          }
-          
-          System.out.print("*");
-          
-          for(i=0; i < stars; i++){
-              System.out.print("*");
-          }
-          
-          System.out.print("\n");
+    public static void starsSpaces(int stars, int spaces){
+        
+        for (int i=0;i<spaces;i++){System.out.print(" ");}
+        
+        for (int i=0; i < stars; i++){
+            System.out.print("*");
+        }
+        System.out.print("*");
+        for (int i=0; i < stars; i++){
+            System.out.print("*");
+        }
+        for (int i=0; i < spaces; i++){
+            System.out.print(" ");
+        }
+        System.out.print("\n");
     }
+    
 }
