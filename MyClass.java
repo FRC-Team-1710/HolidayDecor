411ec6335b3292697347c3e4156f39cf2915f59b
rahidas04 <rahidasgupta04@gmail.cmo>
2020-11-06 16:19:04 -0600

    Christmas Tree Top

--- MyClass.java
+++ MyClass.java
@@ -4,7 +4,29 @@ public class MyClass {
     public static void main(String args[]) {
       
     
+
+	int i = 0;
+	for (i = 0; i < times; i++){
+		row (times - i,i)
     }
+}
+
+	public static void row(int spaces, int stars){
+          int i = 0;
+          for(i = 0; i < spaces; i++){
+              System.out.print(" ");
+          }
+          for(i = 0; i < stars; i++){
+              System.out.print("*");
+          }
+          System.out.print("*");
+          for(i = 0; i < stars; i++){
+              System.out.print("*");
+          }
+          System.out.print("\n");
+    }
+
+
     
 }
      
