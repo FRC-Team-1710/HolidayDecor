2e9606cc7f807cfc131fcb31b802d924a3671de4
GavenB141 <60164067+GavenB141@users.noreply.github.com>
2020-11-06 16:11:41 -0600

    Added Intro

--- MyClass.java
+++ MyClass.java
@@ -5,6 +5,8 @@ public class MyClass {
       
     
     }
-    
+    public static void intro(){
+        System.out.println("Christmas Trees!\nYou will be prompted to enter the number of rows to comprise the height of the tree.\nThe program will then calculate how exactly to make the top half, and then the stump.");
+    }
 }
      
