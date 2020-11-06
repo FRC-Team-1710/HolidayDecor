bbccae947a5916e0699136cfc1478c05e778533c
Andrew S. Kepych <613ask11@stu.olatheschools.org>
2020-11-06 16:07:02 -0600

    Stump Method

--- MyClass.java
+++ MyClass.java
@@ -1,52 +1,28 @@
-import java.util.Scanner;  // Import the Scanner class
-
+import java.util.Scanner;
 public class MyClass {
+    static int i = 0;
     public static void main(String args[]) {
-      Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-    System.out.println("How Many Times?");
-
-    int times = myObj.nextInt();  // Read user input
-    int trunkwidth = Math.round((times * 2 - 1) / 3);
-    int trunkheight = times / 4;
-    int trunkspace = Math.round((times * 2 - 1) / 3 + 1);
-    if ( (trunkwidth & 1) == 0 ) {
-    trunkwidth = trunkwidth -1 ;
-    } 
-    if (times == 5){
-        trunkspace = trunkspace -1;
-    }
-    if (times == 8){
-        trunkspace = trunkspace -1;
-    }
-    for(int i=0; i < times; i++){
-    row(times - i,i * 2 + 1);
-   // trunk(trunkwidth, trunkwidth);
-    }
-    for(int i = 0; i < times / 4; i++){
-    trunk(trunkspace , trunkwidth);
-    }
-    
-    
-    }
-    
-    public static void row(int spaces, int stars){
-         for(int i = 0; i < spaces; i++){
-             System.out.print(" ");
-         }
-    for(int i = 0; i < stars; i++){
-    System.out.print("*");
-    }
-    System.out.print("\n");
-    }
-      public static void trunk(int spaces, int stars){
-          for(int i = -1; i < spaces; i++){
-             System.out.print(" ");
-          }
-           for(int i = 0; i < stars; i++){
-    System.out.print("*");
-    }
-    System.out.print("\n");
+      Scanner myObj = new Scanner(System.in);
+      System.out.println("How Many Times?");
+      int times = myObj.nextInt();
+      
+      for (i=0; i < times; i++){
+       row(i-i/2,i);
+          
       }
-    
+    }
+    public static void row(int spaces, int stars){
+      for (i=0; i < spaces; i++){
+          System.out.print(" ");
+      }
+      for (i=0; i < stars; i++){
+          System.out.print("*");
+      }
+      System.out.print("\n");
 }
-     
+public static void stump(int width, int height){
+    for (i=0; i <= width; i++){
+        System.out.print("*");     //TODO
+    }
+}
+}
