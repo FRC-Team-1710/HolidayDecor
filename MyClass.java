982abb1430832c8d2e1763e57da7ada68e31d4d5
Hudson Hurtig <team1710.cto@gmail.com>
2020-11-06 17:38:38 -0600

    Update MyClass.java

--- MyClass.java
+++ MyClass.java
@@ -24,10 +24,7 @@ public class MyClass {
     Scanner rowScanner = new Scanner(System.in);
     System.out.println("How many rows do you want in the tree height? ==>");
     int rows = rowScanner.nextInt();
-    int i = 0;
-	for (i = 0; i < rows ; i++){
-		row (rows - i, i);
-    }
+    
     return rows;
 
    }
