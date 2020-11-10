9db82b233c94aae0d37600ef11ca37cb2727bd2d
Andrew S. Kepych <613ask11@stu.olatheschools.org>
2020-11-10 16:41:28 -0600

    commit try 3

--- MyClass.java
+++ MyClass.java
@@ -1,7 +1,6 @@
 import java.util.Scanner; //imports scanner class
 
 public class MyClass {
-<<<<<<< HEAD
 
    
     public static void main(String args[]) {
@@ -42,36 +41,13 @@ public class MyClass {
    
    //Printing the favorite foods end
 }
-=======
-   
-    public static void main(String args[]) {
-        getRows();
-        
-    }
-     
-    public static int getRows() {
-    Scanner rowScanner = new Scanner(System.in);
-    System.out.println("How many rows do you want in the tree height? ==>");
-    int rows = rowScanner.nextInt();
-    int i = 0;
-	for (i = 0; i < rows ; i++){
-		row (rows - i, i);
-    }
-    return rows;
-  }
-
->>>>>>> 1d66820c313fecfed4c21b333748c846a11e52a5
 
 	public static void row(int spaces, int stars){
           int i = 0;
           for(i = 0; i < spaces; i++){
               System.out.print(" ");
           }
-<<<<<<< HEAD
  for(i = 0; i < stars; i++){
-=======
-          for(i = 0; i < stars; i++){
->>>>>>> 1d66820c313fecfed4c21b333748c846a11e52a5
               System.out.print("*");
           }
           System.out.print("*");
@@ -79,17 +55,12 @@ public class MyClass {
               System.out.print("*");
           }
           System.out.print("\n");
-<<<<<<< HEAD
  //          stump(5,getRows());
     }
     
     
   public static void stump(int width, int rows){ //Varibles can be changed; Need varible for width
   int i = 0;
-=======
-    }
-  public static void stump(int width, int rows){ //Varibles can be changed; Need varible for width
->>>>>>> 1d66820c313fecfed4c21b333748c846a11e52a5
 for(i=0; i < rows/4; i++){  //I think this is correct but it needs to be tested
     
     for (i=0; i < width/3; i++){
